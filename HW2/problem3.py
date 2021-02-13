0addd940d555bc5e5ea29b1cc930cb4f437f7bfc
Ismail Dayan <iedayan03@gmail.com>
2021-02-13 03:41:34 -0600

    Fixed flipping points function

--- HW2/problem3.py
+++ HW2/problem3.py
@@ -14,9 +14,17 @@ class LogisticRegression(torch.nn.Module):
 
 def flip_points(labels, num_flips):
     new_labels = labels
-    flipped_points_idx = random.sample(range(0, 200), num_flips)
-    for idx in flipped_points_idx:
-        new_labels[idx] = 1 - new_labels[idx]   # Flips 0 to 1 and 1 to 0
+    positive_flips = 0
+    negative_flips = 0
+    for i in range(len(new_labels)):
+        if positive_flips == num_flips and negative_flips == num_flips:
+            break
+        if positive_flips != num_flips and new_labels[i] == 1:
+            new_labels[i] = 0
+            positive_flips += 1
+        elif negative_flips != num_flips and new_labels[i] == 0:
+            new_labels[i] = 1
+            negative_flips += 1
     return new_labels
 
 def logistic_regression(points, labels):
@@ -29,7 +37,6 @@ def logistic_regression(points, labels):
         loss = cost_fn(outputs, labels)
         loss.backward()
         sgd_optimizer.step()
-
     return model.parameters()
 
 def main():
@@ -47,38 +54,16 @@ def main():
     random_points = random_points.reshape(-1, 1)
     labels_no_flips = torch.tensor(labels)
 
-    [m, b] = logistic_regression(random_points, labels_no_flips)
-    print("Transition Layer (No flips): ", 1 / m.data[0][0].item())
+    transition_layer_widths = []
+    num_flips = [0, 5, 15, 20, 25, 30, 35]
+    for num_flip in num_flips:
+        flipped_labels = flip_points(labels, num_flip)
+        flipped_labels = torch.tensor(flipped_labels)
+        [m, b] = logistic_regression(random_points, flipped_labels)
+        transition_layer_widths.append(1 / m.data[0][0].item())
 
-    five_flipped_labels = flip_points(labels, 5)
-    five_flipped_labels = torch.tensor(five_flipped_labels)
-    [m, b] = logistic_regression(random_points, five_flipped_labels)
-    print("Transition Layer (5 flips): ", 1 / m.data[0][0].item())
-
-    fifteen_flipped_labels = flip_points(labels, 15)
-    fifteen_flipped_labels = torch.tensor(fifteen_flipped_labels)
-    [m, b] = logistic_regression(random_points, fifteen_flipped_labels)
-    print("Transition Layer (15 flips): ", 1 / m.data[0][0].item())
-
-    twenty_flipped_labels = flip_points(labels, 15)
-    twenty_flipped_labels = torch.tensor(twenty_flipped_labels)
-    [m, b] = logistic_regression(random_points, twenty_flipped_labels)
-    print("Transition Layer (20 flips): ", 1 / m.data[0][0].item())
-
-    twentyfive_flipped_labels = flip_points(labels, 15)
-    twentyfive_flipped_labels = torch.tensor(twentyfive_flipped_labels)
-    [m, b] = logistic_regression(random_points, twentyfive_flipped_labels)
-    print("Transition Layer (25 flips): ", 1 / m.data[0][0].item())
-
-    thirty_flipped_labels = flip_points(labels, 15)
-    thirty_flipped_labels = torch.tensor(thirty_flipped_labels)
-    [m, b] = logistic_regression(random_points, thirty_flipped_labels)
-    print("Transition Layer (30 flips): ", 1 / m.data[0][0].item())
-
-    thirtyfive_flipped_labels = flip_points(labels, 15)
-    thirtyfive_flipped_labels = torch.tensor(thirtyfive_flipped_labels)
-    [m, b] = logistic_regression(random_points, thirtyfive_flipped_labels)
-    print("Transition Layer (35 flips): ", 1 / m.data[0][0].item())
+    plt.plot(num_flips, transition_layer_widths)
+    plt.show()
 
 if __name__ == "__main__":
     main()
