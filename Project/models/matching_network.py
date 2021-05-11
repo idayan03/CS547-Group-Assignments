c97b84f9f89205fdd0041802966769d8b2502e2f
Ryan Estep <restep2@illinois.edu>
2021-05-11 15:31:12 -0500

    matching yaml and fixes

--- Project/models/matching_network.py
+++ Project/models/matching_network.py
@@ -1,72 +1,52 @@
 import torch
 import numpy as np
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 import math
 
 class MatchingNet(nn.Module):
-    def __init__(self, in_channels=1, layer_dim = 64):
-        super(MatchingNetwork, self).__init__()
+    def __init__(self, input_dim, conv_channels, num_conv_layers):
+        super().__init__()
 
-    self.conv_layers = []
-    for i in range(4):
-        if(i==0):
-            input_dim = in_channels
-        else:
-             input_dim = layer_dim
-        conv_layer = nn.Sequential(nn.Conv2d(input_dim, layer_dim, 3, 1, 1, bias=True)
-                                   nn.BatchNorm2d(out_channels)
-                                   nn.ReLU(True)
-                                   nn.MaxPool2d(2, 2))
-        self.conv_layers.append(conv_layer)
-    self.conv_layers = nn.Sequential(*self.conv_layers)
+        self.conv_layers = []
+        for i in range(num_conv_layers):
+            conv_layer = nn.Sequential(nn.Conv2d(input_dim[-1] if i == 0 else conv_channels, conv_channels, 3, 1, 1, bias=True),
+                                       nn.BatchNorm2d(conv_channels),
+                                       nn.ReLU(True),
+                                       nn.MaxPool2d(2, 2))
+            self.conv_layers.append(conv_layer)
+        self.conv_layers = nn.Sequential(*self.conv_layers)
 
-    self.softmax = nn.Softmax()
+        self.cos = nn.CosineSimilarity(dim=1, eps=1e-10)
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, support_set_images, s_set_one_hots, target_image, target_label):
-        encodings = []
-        for i in np.arange(support_set_images.size(1)):
-            x = self.conv_layers(support_set_images[:,i,:,:,:])
-            x = x.view(x.size(0), -1)
-            encodings.append(x)
+        self.cross_loss = nn.CrossEntropyLoss()
 
-        # produce embeddings for target images
-        for i in np.arange(target_image.size(1)):
-            x = self.conv_layers(target_image[:,i,:,:,:])
-            x = x.view(x.size(0), -1)
-            encodings.append(x)
-            outputs = torch.stack(encodings)
+    def forward(self, s_x, s_y, q_x, q_y):
+        s_embeds = self.conv_layers(s_x.permute(0, 3, 1, 2))
+        s_embeds = s_embeds.view(s_embeds.size(0), -1)
 
-            # distnet
-            s_embeds = outputs[:-1]
-            target_embed = outputs[-1]
-            cos_sims = []
-            for image in s_embeds:
-                sum_s_embeds = np.sum(image**2, 1)
-                s_mags = np.recipricol(np.sqrt(np.clip(sum_s_embeds, 1e-8, np.inf)))
-                s_mags = from_numpy(s_mags)
-                dot = bmm(target_embed.unsqueeze(1), image.unsqueeze(2)).squeeze()
-                cos_sims.append(dot * s_mags)
-            cos_sims = torch.stack(cos_sims)
-            cos_sims = transpose(cos_sims, 0, 1)
+        q_embeds = self.conv_layers(q_x.permute(0, 3, 1, 2))
+        q_embeds = q_embeds.view(q_embeds.size(0), -1)
 
-            # attention
-            softmax_sims = self.softmax(cos_sims)
-            pred_y = bmm(softmax_sims.unsqueeze(1), s_set_one_hots).squeeze()
+        s_embeds_rep = s_embeds.repeat(q_x.shape[0], 1)
+        #0, 1, 2, 0, 1, 2 ...
 
-            # crossentropy loss
-            values, indices = max(pred_y, 1)
-            if i == 0:
-                accuracy = torch.mean((indices.squeeze() == target_label[:,i]).float())
-                loss = F.cross_entropy(pred_y, target_label[:,i].long())
-            else:
-                accuracy = accuracy + torch.mean((indices.squeeze() == target_label[:, i]).float())
-                loss = loss + F.cross_entropy(pred_y, target_label[:, i].long())
+        q_embeds_rep = q_embeds.repeat_interleave(s_x.shape[0], dim=0)
+        #0, 0, 0, 1, 1, 1, ...
 
-            # delete the last target image encoding of encoded_images
-            encodings.pop()
+        cos_sims = self.cos(q_embeds_rep, s_embeds_rep)
 
-            loss = loss/target_image.size(1)
-            faccuracy = accuracy/target_image.size(1)
+        # attention
+        scores = self.softmax(cos_sims.reshape(q_x.shape[0], s_x.shape[0]))
+
+        loss = self.cross_loss(scores, q_y)
+
+        #find best support sample for each query
+        best_support = scores.argmax(-1)
+
+        #predict class
+        pred_y = s_y[best_support]
 
         return loss, pred_y
